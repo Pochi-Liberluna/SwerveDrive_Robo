8a0af7cdb4db21739db60a88f2e4e2d6c291e452
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-06-13 13:14:36 +0900

    Update LED.ino

--- 並列処理Test/LED.ino
+++ 並列処理Test/LED.ino
@@ -1,12 +1,49 @@
-#define LED1Pin 9
-#define LED2Pin 10
 #define OUTPUT OPT  //DigitalWrite用 OUTPUT定数
 #define INPUT IPT   //DigitalWrite用 INPUT定数
+#define HIGH H      //DigitalWrite用 HIGH定数
+#define LOW L       //DigitalWrite用 LOW定数
+
+class MultiLED{
+  int LEDPin;
+  long OnTime;
+  long OffTime;
+
+  int LEDState;
+  unsigned long previousMillis;
+
+  public:
+  On(int Pin, long On, long Off){
+    LEDPin = Pin;
+    pinMode(LEDPin, OPT);
+
+    OnTime = On;
+    OffTime = Off;
+
+    LEDState = L
+    previousMillis = 0;
+  }
+void On(){
+  unsigned long currentMillis = millis();
+
+  if((LEDState == H) && (currentMillis - previousMillis >= OnTime)){
+    LESDtate = L;
+    previousMillis = currentMillis;
+    digitalWrite(LEDPin, LEDState);
+  } else if((LEDState == L) && (currentMillis - previousMillis >= OffTime)){
+    LEDState = H;
+    previousMillis = currentMillis;
+    digitalWrite(LEDPin, LEDState);
+  }
+}
+};
+
+MultiLED LED1(9, 250, 750);
+MultiLED LED2(10, 330, 400);
 
 void setup(){
-  pinMode(LED1Pin, OPT);
-  pinMode(LED2Pin, OPT);
+
 }
 void loop(){
-  
+ LED1.On();
+ LED2.On();
 }
