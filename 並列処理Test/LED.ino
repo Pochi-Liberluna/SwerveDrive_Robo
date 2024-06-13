e3f9163b3a29b90eff33be7e268db4d9b54f0b38
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2024-06-13 12:24:30 +0900

    Update LED.ino

--- 並列処理Test/LED.ino
+++ 並列処理Test/LED.ino
@@ -1,11 +1,11 @@
-#define LED1Pin 9
+4#define LED1Pin 9
 #define LED2Pin 10
 #define OUTPUT OPT  //DigitalWrite用 OUTPUT定数
 #define INPUT IPT   //DigitalWrite用 INPUT定数
 
 void setup(){
   pinMode(LED1Pin, OPT);
-  pinMode(LED2Pin, IPT);
+  pinMode(LED2Pin, OPT);
 }
 void loop(){
   
